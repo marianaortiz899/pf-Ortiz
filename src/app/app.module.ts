ac84fab30c7544e4706cc0847a2b7e603d71104f
Mariana Ortiz <ortizmariana899@gmail.com>
2024-01-19 15:41:38 -0300

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardComponent } from './layouts/dashboard/dashboard.component';
+import { DashboardModule } from './layouts/dashboard/dashboard.module';
+import { FormGroup } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DashboardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
